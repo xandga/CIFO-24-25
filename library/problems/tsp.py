38758bf193488271467ff3c81a01af4f5c9473a1
Inês Magessi <ines.magessi@tecnico.ulisboa.pt>
2025-03-23 18:10:20 +0000

    TSP and KS Problems

---
+++ library/problems/tsp.py
@@ -0,0 +1,83 @@
+"""Problem definition
+
+Description: The Traveling Salesperson Problem (TSP) is the challenge of finding the shortest possible route that starts in a given city, visits each of the remaining N-1 cities exactly once, and returns to the starting city.
+
+Search space: All possible permutations of city visit orders, forming valid round-trip routes.
+
+Representation: List of city indexes that compose the route
+
+Fitness function: f(x)= Total distance traveled, computed as the sum of distances between consecutive cities in the route.
+
+Neighbors: A neighbor solution is obtained by swapping the positions of two consecutive cities in the route.
+
+Goal: Minimize f(x).
+"""
+import random
+from copy import deepcopy
+
+from library.solution import Solution
+
+from library.problems.data.tsp_data import distance_matrix
+
+class TSPSolution(Solution):
+    """The Travel Salesperson Problem (TSP) aims at finding the shortest possible route that starts and
+    ends in a given city and visits all other cities once."""
+    def __init__(
+        self,
+        repr = None,
+        distance_matrix: list[list[float]] = distance_matrix,
+        starting_idx: int = 0
+    ):
+        self.starting_idx = starting_idx
+        self.distance_matrix = distance_matrix
+
+        # Validate repr if it is passed as argument
+        if repr:
+            self._validate_repr(repr)
+
+        super().__init__(repr=repr)
+    
+    def _validate_repr(self, repr):
+        # Confirm repr is list
+        if not isinstance(repr, list):
+            raise TypeError('Representation must be a list')
+        # Make sure repr is a list of integers
+        elif not all([isinstance(idx, int) for idx in repr]):
+            raise TypeError('Representation must be a list of integers (indexes of the route)')
+        # Validate start and end route points
+        if (repr[0] != self.starting_idx) or (repr[-1] != self.starting_idx):
+            raise ValueError("TSP route must start and end in the starting index")
+        # Validate route length and content
+        if (len(repr) != (len(distance_matrix) + 1)) or (set(repr) != set([i for i in range(len(distance_matrix))])):
+            raise ValueError("TSP route must pass through all cities")
+
+    def random_initial_representation(self):
+        # Route starts in starting idx
+        route = [self.starting_idx]
+        # Get city idx to visit and shuffle them
+        idx_to_visit = [idx for idx in range(len(self.distance_matrix)) if idx != self.starting_idx]
+        random.shuffle(idx_to_visit)
+        # Add idx to visit to route
+        route = route + idx_to_visit
+        # Route ends in starting idx
+        route = route + [self.starting_idx]
+        return route
+    
+    def fitness(self):
+        total_distance = 0
+        for i in range(len(self.repr)-1):
+            total_distance += self.distance_matrix[self.repr[i]][self.repr[i+1]]
+        return total_distance
+
+
+class TSPHillClimbingSolution(TSPSolution):
+    def get_neighbors(self):
+        """Neighbors are obtained by swaping the positions of two consecutuve cities"""
+        neighbors = []
+        for i in range(1, len(self.repr)-2):
+            new_route = deepcopy(self.repr)
+            new_route[i], new_route[i+1] = new_route[i+1], new_route[i]
+            neighbor = TSPHillClimbingSolution(route=new_route, distance_matrix=self.distance_matrix)
+            neighbors.append(neighbor)
+
+        return neighbors
